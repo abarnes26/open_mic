13d17782bf0800578f8d8dd1c354165fd0700e22
abarnes26 <abarnes26@yahoo.com>
2017-10-25 09:21:10 -0600

    completed iteration 2

--- test/user_test.rb
+++ test/user_test.rb
@@ -2,6 +2,7 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/user'
+require './lib/joke'
 
 class UserTest < Minitest::Test
   def test_it_exists
@@ -21,4 +22,22 @@ class UserTest < Minitest::Test
 
     assert_equal "Ali", ali.name
   end
+
+  def test_user_has_no_jokes
+    sal = User.new("Sal")
+
+    assert_equal [], sal.jokes
+  end
+
+  def test_user_can_learn_jokes
+    sal = User.new("Sal")
+    joke = Joke.new({id: 1,
+               question: "Why did the strawberry cross the road?",
+                 answer: "Because his mother was in a jam."})
+    sal.learn(joke)
+
+    assert_instance_of Joke, sal.jokes.first
+    assert_equal 1, sal.jokes.count
+  end
+
 end
