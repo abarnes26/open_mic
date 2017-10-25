35ee2a54c7f57553427ede7f908f27f418f68e9e
abarnes26 <abarnes26@yahoo.com>
2017-10-25 09:16:18 -0600

    Through iteration 1

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,3 +1,11 @@
 class Joke
 
+  attr_reader :id, :question, :answer
+
+  def initialize(question_answer)
+    @id = question_answer[:id]
+    @question = question_answer[:question]
+    @answer = question_answer[:answer]
+  end
+
 end
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -5,4 +5,35 @@ require './lib/joke'
 
 class JokeTest < Minitest::Test
 
+  def test_joke_instantiates
+    joke = Joke.new({id: 1,
+               question: "Why did the strawberry cross the road?",
+                 answer: "Because his mother was in a jam."})
+
+     assert_instance_of Joke, joke
+  end
+
+  def test_joke_has_an_id
+    joke = Joke.new({id: 1,
+               question: "Why did the strawberry cross the road?",
+                 answer: "Because his mother was in a jam."})
+
+    assert_equal 1, joke.id
+  end
+
+  def test_joke_has_a_question
+    joke = Joke.new({id: 1,
+               question: "Why did the strawberry cross the road?",
+                 answer: "Because his mother was in a jam."})
+
+   assert_equal "Why did the strawberry cross the road?", joke.question
+  end
+
+  def test_the_joke_has_an_answer
+    joke = Joke.new({id: 1,
+               question: "Why did the strawberry cross the road?",
+                 answer: "Because his mother was in a jam."})
+
+   assert_equal "Because his mother was in a jam.", joke.answer
+ end
 end
